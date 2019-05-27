f1a7cbd8692e38c26677b1d9c14b741e87552327
judith <judithcynthia1@gmail.com>
2019-05-27 16:12:27 +0100

    Made some changes on the responsiveness

--- src/components/PayForRide.vue
+++ src/components/PayForRide.vue
@@ -66,7 +66,7 @@ export default {
                         response.tx.chargeResponseCode == "0"
                     ) {
                         // redirect to a success page
-                        location.replace("/redirect");
+                        location.replace("/Redirect");
                     } else {
                         // redirect to a failure page.
                     }
